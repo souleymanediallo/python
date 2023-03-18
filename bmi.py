4a1863060046fcfd3d51ff6e6587bcdb730b966e
souleymanediallo <souleymanedialloo@yahoo.fr>
2023-03-18 17:00:48 +0100

    add bmi

---
+++ bmi.py
@@ -0,0 +1,7 @@
+height = float(input("enter your height in m : "))
+weight = int(input("enter your weight in kg : "))
+
+result =  weight / (height ** 2 )
+result_as_int = int(result)
+print(result_as_int)
+
