03718c84de630632e04c4f44b2871daf4ce145f4
souleymanediallo <souleymanedialloo@yahoo.fr>
2024-01-30 22:53:49 +0100

    exo

---
+++ intermediate/project_22/exo_6.py
@@ -0,0 +1,13 @@
+# def details(name, age, city):
+#     print(f"Je m'appelle {name}, j'ai {age} ans et j'habite à {city}.")
+
+
+
+def details(name, age, city):
+    print(f"Je m'appelle {name}, j'ai {age} ans et j'habite à {city}.")
+    return f"Je m'appelle {name}, j'ai {age} ans et j'habite à {city}."
+
+details('John', 18, 'Paris')
+details('Jane', 20, 'Lyon')
+
+#print(details('John', 18, 'Paris'))
